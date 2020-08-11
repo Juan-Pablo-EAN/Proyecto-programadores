4bfb55a74876108f5810def1521a31b117ec5b29
Juan Pablo <jballes89830@universidadean.edu.co>
2020-08-11 17:28:18 -0500

    condicionales web y multiplataforma

--- src/proyectoprogramadores/ProyectoProgramadores.java
+++ src/proyectoprogramadores/ProyectoProgramadores.java
@@ -11,6 +11,10 @@ public class ProyectoProgramadores {
 
         int base = 3000000;
         int eleccion = 0;
+        int precio = 0;
+        int tiempo = 0;
+        String moneda = "";
+        String tipo = "";
 
         System.out.println("Bienvenido programador");
         System.out.println("¿Qué tipo de aplicación vas a desarrollar?");
@@ -19,9 +23,37 @@ public class ProyectoProgramadores {
         System.out.println("3. Desarrollo web");
         System.out.println("4. multiplataforma");
         eleccion = entrada.nextInt();
-        if(eleccion == 1){
-             
+        if (eleccion == 1) {
+
+        } else if (eleccion == 3) {
+            precio = base + 1000000 + 250000 + 42000;
+            tiempo = 4;
+            tipo = "pagina web";
+            
+        } else if (eleccion == 4) {
+            precio = base + 5000000;
+            tiempo = 6;
+            tipo = "aplicación multiplataforma";
         }
+        System.out.println("¿Deseas el precio en pesos colombianos, dolares o euros?");
+        System.out.println("1. Pesos colombianos");
+        System.out.println("2. Dolares");
+        System.out.println("3. Euros");
+        eleccion = entrada.nextInt();
+        if (eleccion == 1) {
+            precio = precio;
+            moneda = "pesos";
+        } else if (eleccion == 2) {
+            precio = precio/3772;
+            moneda = "dolares";
+        } else if (eleccion == 3) {
+            precio = precio/4428;
+            moneda = "euros";
+        } else {
+            System.out.println("Ingresaste un valor incorrecto");
+        }
+
+        System.out.println("El costo de una " + tipo + " es de $" + precio + " " + moneda + " y tardaría " + tiempo + " meses.");
     }
 
 }
