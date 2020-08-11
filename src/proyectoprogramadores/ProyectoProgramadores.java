438fb53de38f962a96e41aa9fec6441b1672b7f0
Juan Pablo <jballes89830@universidadean.edu.co>
2020-08-11 17:54:57 -0500

    comentarios

--- src/proyectoprogramadores/ProyectoProgramadores.java
+++ src/proyectoprogramadores/ProyectoProgramadores.java
@@ -25,12 +25,12 @@ public class ProyectoProgramadores {
         eleccion = entrada.nextInt();
         if (eleccion == 1) {
 
-        } else if (eleccion == 3) {
+        } else if (eleccion == 3) { //inicio de la condicion de paginas web
             precio = base + 1000000 + 250000 + 42000;
             tiempo = 4;
             tipo = "pagina web";
             
-        } else if (eleccion == 4) {
+        } else if (eleccion == 4) { //inicio de la condicion de app multiplataforma
             precio = base + 5000000;
             tiempo = 6;
             tipo = "aplicación multiplataforma";
@@ -44,10 +44,10 @@ public class ProyectoProgramadores {
             precio = precio;
             moneda = "pesos";
         } else if (eleccion == 2) {
-            precio = precio/3772;
+            precio = precio/3772; //conversion a dolares
             moneda = "dolares";
         } else if (eleccion == 3) {
-            precio = precio/4428;
+            precio = precio/4428; //conversion a euros
             moneda = "euros";
         } else {
             System.out.println("Ingresaste un valor incorrecto");
